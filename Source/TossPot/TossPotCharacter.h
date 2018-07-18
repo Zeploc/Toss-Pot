1cfe3efbfbad237142a1491b89e813469dd1971c
Zeploc <Zeploc@users.noreply.github.com>
2018-07-18 15:22:09 +1200

    Completed Base Camera Movement

--- Source/TossPot/TossPotCharacter.h
+++ Source/TossPot/TossPotCharacter.h
@@ -11,18 +11,14 @@ class ATossPotCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
-	/** Side view camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* SideViewCameraComponent;
-
-	/** Camera boom positioning the camera beside the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
 
 protected:
 
 	/** Called for side to side input */
 	void MoveRight(float Val);
+
+	/** Called for side to side input */
+	void MoveUp(float Val);
 
 	/** Handle touch inputs. */
 	void TouchStarted(const ETouchIndex::Type FingerIndex, const FVector Location);
@@ -38,8 +34,4 @@ protected:
 public:
 	ATossPotCharacter();
 
-	/** Returns SideViewCameraComponent subobject **/
-	FORCEINLINE class UCameraComponent* GetSideViewCameraComponent() const { return SideViewCameraComponent; }
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 };
