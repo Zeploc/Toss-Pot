cdfa1cc0f3c617f6da3033d6182022abc96ea590
Zeploc <alexcoultasis@gmail.com>
2018-10-31 17:15:00 +1300

    - Added Door - Added Animation to Door BP - Added Collision to Door - Set Doors open on level select screen - Added 3 entries to level select

--- Source/TossPot/Triggers/DoorTrigger.cpp
+++ Source/TossPot/Triggers/DoorTrigger.cpp
@@ -11,54 +11,51 @@ ADoorTrigger::ADoorTrigger()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	Door = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door"));
-	DoorFrame = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door Frame"));
-	Door->SetCollisionProfileName("BlockAll");
-	DoorFrame->SetCollisionProfileName("OverlapAll");
-	SetRootComponent(DoorFrame);
+	Door = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Door"));
+	//Door->SetCollisionProfileName("BlockAll");
+	SetRootComponent(Door);
 }
 
 void ADoorTrigger::BeginPlay()
 {
 	Super::BeginPlay();
-	StartPosition = Door->GetComponentLocation();// GetActorLocation();
 }
 
 void ADoorTrigger::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	FVector CurrentPos = Door->GetComponentLocation();// GetActorLocation();
-	if (bOpen)
-	{
-		if (FVector::Distance(CurrentPos, StartPosition) < MoveDistance)
-		//if (CurrentPos.Z < StartPosition.Z + MoveDistance)
-		{
-			CurrentPos += GetActorUpVector() * MoveSpeed * DeltaTime;
-			Door->SetWorldLocation(CurrentPos);// SetActorLocation(CurrentPos);
-		}
-		else
-		{
-			FVector NewPosition = StartPosition;
-			NewPosition += GetActorUpVector() * MoveDistance;
-			Door->SetWorldLocation(NewPosition);  //SetActorLocation(NewPosition);
-			bOpen = false;
-		}
-	}
-	else if (bClose)
-	{
-		if (FVector::DotProduct(CurrentPos - StartPosition, GetActorUpVector()) > 0)
-		//if (CurrentPos.Z > StartPosition.Z)
-		{
-			CurrentPos -= GetActorUpVector() * MoveSpeed * DeltaTime;
-			Door->SetWorldLocation(CurrentPos); //SetActorLocation(CurrentPos);
-		}
-		else
-		{
-			bClose = false;
-			Door->SetWorldLocation(StartPosition);  //SetActorLocation(StartPosition);
-		}
-	}
+	//FVector CurrentPos = Door->GetComponentLocation();// GetActorLocation();
+	//if (bOpen)
+	//{
+	//	if (FVector::Distance(CurrentPos, StartPosition) < MoveDistance)
+	//	//if (CurrentPos.Z < StartPosition.Z + MoveDistance)
+	//	{
+	//		CurrentPos += GetActorUpVector() * MoveSpeed * DeltaTime;
+	//		Door->SetWorldLocation(CurrentPos);// SetActorLocation(CurrentPos);
+	//	}
+	//	else
+	//	{
+	//		FVector NewPosition = StartPosition;
+	//		NewPosition += GetActorUpVector() * MoveDistance;
+	//		Door->SetWorldLocation(NewPosition);  //SetActorLocation(NewPosition);
+	//		bOpen = false;
+	//	}
+	//}
+	//else if (bClose)
+	//{
+	//	if (FVector::DotProduct(CurrentPos - StartPosition, GetActorUpVector()) > 0)
+	//	//if (CurrentPos.Z > StartPosition.Z)
+	//	{
+	//		CurrentPos -= GetActorUpVector() * MoveSpeed * DeltaTime;
+	//		Door->SetWorldLocation(CurrentPos); //SetActorLocation(CurrentPos);
+	//	}
+	//	else
+	//	{
+	//		bClose = false;
+	//		Door->SetWorldLocation(StartPosition);  //SetActorLocation(StartPosition);
+	//	}
+	//}
 }
 
 void ADoorTrigger::Trigger()
@@ -67,8 +64,8 @@ void ADoorTrigger::Trigger()
 	/*FVector NewPosition = StartPosition;
 	NewPosition.Z += MoveDistance;
 	SetActorLocation(NewPosition);*/
-	bOpen = true;
-	bClose = false;
+	//bOpen = true;
+	//bClose = false;
 	if (NumOfTriggered == 0) UGameplayStatics::PlaySoundAtLocation(this, MoveSound, GetActorLocation());
 	NumOfTriggered++;
 	GEngine->AddOnScreenDebugMessage(-1, 5.000f, FColor::Orange, TEXT("[Enable] Current Door Number " + FString::SanitizeFloat(NumOfTriggered)));
@@ -81,8 +78,8 @@ void ADoorTrigger::DisableTrigger()
 	GEngine->AddOnScreenDebugMessage(-1, 5.000f, FColor::Orange, TEXT("[Disable] Current Door Number " + FString::SanitizeFloat(NumOfTriggered)));
 	if (NumOfTriggered == 0)
 	{
-		bOpen = false;
-		bClose = true;
+		//bOpen = false;
+		//bClose = true;
 		UGameplayStatics::PlaySoundAtLocation(this, MoveSound, GetActorLocation());
 		//SetActorLocation(StartPosition);
 	}
